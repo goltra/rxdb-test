4e14da0328b1545b1fbf45b2e596294497b7bf05
goltra <goltratec@gmail.com>
2017-02-22 08:37:35 +0100

    .

--- src/providers/dbmanager.ts
+++ src/providers/dbmanager.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import * as RxDB from 'rxdb';
+import PouchDB from 'pouchdb';
+
 
 /*
   Generated class for the Dbmanager provider.
@@ -11,15 +12,99 @@ import * as RxDB from 'rxdb';
 */
 @Injectable()
 export class Dbmanager {
+  data: any;
+  db: any;
+  remote: any;
 
   constructor(public http: Http) {
-    console.log('Hello Dbmanager Provider');
-    RxDB.create(
-      'heroesDB',
-      'websql',
-      '123456789012',
-      true
-    ).then(db=>console.dir(db));
+    this.db = new PouchDB('cloudo');
+    this.remote = 'http://localhost:5984/pruebasdb'
+
+    let options = {
+      live: true,
+      retry: true,
+      continuous: true,
+    }
+
+    this.db.sync(this.remote, options);
+  }
+  getTodos() {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+      this.db.allDocs({
+
+        include_docs: true
+
+      }).then((result) => {
+
+        this.data = [];
+
+        let docs = result.rows.map((row) => {
+          this.data.push(row.doc);
+        });
+
+        resolve(this.data);
+
+        this.db.changes({ live: true, since: 'now', include_docs: true }).on('change', (change) => {
+          this.handleChange(change);
+        });
+
+      }).catch((error) => {
+
+        console.log(error);
+
+      });
+    });
+  }
+  createTodo(todo) {
+    this.db.post(todo);
   }
 
+  updateTodo(todo) {
+    this.db.put(todo).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  deleteTodo(todo) {
+    this.db.remove(todo).catch((err) => {
+      console.log(err);
+    });
+  }
+  handleChange(change) {
+
+    let changedDoc = null;
+    let changedIndex = null;
+
+    this.data.forEach((doc, index) => {
+
+      if (doc._id === change.id) {
+        changedDoc = doc;
+        changedIndex = index;
+      }
+
+    });
+
+    //A document was deleted
+    if (change.deleted) {
+      this.data.splice(changedIndex, 1);
+    }
+    else {
+
+      //A document was updated
+      if (changedDoc) {
+        this.data[changedIndex] = change.doc;
+      }
+
+      //A document was added
+      else {
+        this.data.push(change.doc);
+      }
+
+    }
+
+  }
 }
