27577a967028e48979af3408ceddb83c347b13f9
goltra <goltratec@gmail.com>
2017-02-23 08:40:21 +0100

    .

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { Dbmanager } from '../../providers/dbmanager';
 
 @Component({
@@ -9,8 +9,78 @@ import { Dbmanager } from '../../providers/dbmanager';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public db: Dbmanager) {
-
+  todos: any;
+ 
+  constructor(public navCtrl: NavController, public todoService: Dbmanager, public alertCtrl: AlertController) {
+ 
   }
-
+ 
+  ionViewDidLoad(){
+ 
+    this.todoService.getTodos().then((data) => {
+      this.todos = data;
+    });
+ 
+  }
+ 
+  createTodo(){
+ 
+    let prompt = this.alertCtrl.create({
+      title: 'Add',
+      message: 'What do you need to do?',
+      inputs: [
+        {
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.todoService.createTodo({title: data.title});
+          }
+        }
+      ]
+    });
+ 
+    prompt.present();
+ 
+  }
+ 
+  updateTodo(todo){
+ 
+    let prompt = this.alertCtrl.create({
+      title: 'Edit',
+      message: 'Change your mind?',
+      inputs: [
+        {
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.todoService.updateTodo({
+              _id: todo._id,
+              _rev: todo._rev,
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+ 
+    prompt.present();
+  }
+ 
+  deleteTodo(todo){
+    this.todoService.deleteTodo(todo);
+  }
 }
